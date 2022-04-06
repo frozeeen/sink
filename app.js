96f790b44df8824b17370aee4d1b19d0c926d07b
frozeeen <florenciobagayawa@gmail.com>
2022-04-06 20:35:33 +0800

    The Dashboard is now inserted via JavaScript

--- app.js
+++ app.js
@@ -87,3 +87,72 @@ async function sink_now(url, param, config){
 
 	console.log("Finish sinking! Total:", config.count);
 }
+
+// INSERT STYLE
+var SINK_STYLE = `<style>
+#sink{
+	position: absolute; bottom: 0; max-width: 450px; left: 5%;
+	padding: 1rem;
+	border-top-left-radius: 1rem;
+	border-top-right-radius: 1rem;
+	background: #0d1117;
+	color: white;
+	border: 1px solid white;
+	border-bottom: 0;
+}
+#sink *{ box-sizing: border-box; }
+#sink{
+	background: #0d1117; color: white; font-family: "Open sans", "Arial";
+}
+#sink input, #sink textarea{
+	font-family: inherit; font-size: inherit; background: none; color: inherit; width: 100%;
+	margin-bottom: 1rem; padding: 0.5rem;
+	border: 1px solid #4f4f4f;
+	border-radius: 0.2rem;
+}
+#sink input[type="submit"]{ background: white; color: #0d1117; font-weight: bold; border: none; }
+#sink label{ font-size: small; color: gray; }
+#sink main{ width: 500px; margin: 3rem auto 0rem; }
+#sink #header{ text-align: center; margin-bottom: 2rem; }
+#sink #header h2{ margin: 0rem; }
+</style>`;
+document.head.insertAdjacentHTML("beforeend", SINK_STYLE);
+
+
+// INSERT THE DASHBOARD
+var SINK_DASHBOARD = `<div id="sink" >
+	<div id="header">
+		<h2>Sink</h2>
+		<div>Fish generator for Phishers</div>
+	</div>
+	<label>Target URL</label>
+	<input id="link" type="text" placeholder="Link" value="http://localhost/sink/3vilBoat/login.php" >
+	<label>Parameters</label>
+	<textarea id="params" rows="5" >{
+	"handle": "!EMAIL",
+	"password": "!PASSWORD",
+	"planet": "earth"
+}</textarea>
+	<label>Fish count</label>
+	<input id="count" type="number" value="100" >
+	<input id="submitButton" type="submit" value="Sink now!" >
+	<div>
+		<div>Fish generated <span id="fishGenerated" >0</span></div>
+	</div>
+</div>`;
+document.body.insertAdjacentHTML("beforeend", SINK_DASHBOARD);
+
+// ADD THE EVENTS
+var linkTarget = document.querySelector("#sink #link");
+var linkParams = document.querySelector("#sink #params");
+var fishCount  = document.querySelector("#sink #count");
+var submitButton = document.querySelector("#sink #submitButton");
+var fishGenerated = document.querySelector("#sink #fishGenerated");
+submitButton.addEventListener("click", () => {
+	sink_now(linkTarget.value, JSON.parse(linkParams.value), {
+		count: fishCount.value,
+		every_generate: function(data){
+			fishGenerated.innerHTML = `${ data.count } / ${ fishCount.value }`;
+		}
+	});
+});
