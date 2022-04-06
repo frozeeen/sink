5cb5796e6489ff987bb7dcc9e56b0dc170c8ada1
frozeeen <florenciobagayawa@gmail.com>
2022-04-06 20:39:48 +0800

    Now using the 3vilBoat as the main testing and designing page

--- 3vilBoat/index.php
+++ 3vilBoat/index.php
@@ -32,5 +32,7 @@
 		<input type="password" name="password" placeholder="Password" />
 		<input type="submit" value="Submit" />
 	</form>
+
+	<script type="text/javascript" src="../app.js" ></script>
 </body>
 </html>
